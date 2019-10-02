9e19c197b5215bd7a57f63fcdb82cd41666cd49e
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2019-10-02 18:07:07 -0300

    customers component

---
+++ src/app/customers/customers-list/filter-textbox.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-filter-textbox',
+    templateUrl: './filter-textbox.component.html'
+})
+export class FilterTextboxComponent implements OnInit {
+
+    ngOnInit() {
+
+    }
+
+}
