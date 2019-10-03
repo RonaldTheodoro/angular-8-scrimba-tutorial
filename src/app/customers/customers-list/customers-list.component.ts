bab02f2ef5d1d109ff7449da2f330fb5ce6bb27a
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2019-10-03 11:34:50 -0300

    add sort

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,20 +1,49 @@
 import { Component, OnInit } from "@angular/core";
 
-import { ICustomer } from 'src/app/shared/interfaces';
+import { ICustomer } from "src/app/shared/interfaces";
 
 @Component({
   selector: "app-customers-list",
-  templateUrl: "./customers-list.component.html",
+  templateUrl: "./customers-list.component.html"
 })
 export class CustomersListComponent implements OnInit {
   filteredCustomers: ICustomer[] = [];
   customersOrderTotal: number;
-  currencyCode: string = 'USD';
+  currencyCode: string = "USD";
 
   constructor() {}
 
   ngOnInit() {
-
+    this.filteredCustomers = [
+      {
+        id: 1,
+        name: "john Doe",
+        city: "Phoenix",
+        orderTotal: 9.99,
+        customerSince: new Date(2014, 7, 10)
+      },
+      {
+        id: 2,
+        name: "Jane Doe",
+        city: "Chandler",
+        orderTotal: 19.99,
+        customerSince: new Date(2017, 2, 22)
+      },
+      {
+        id: 3,
+        name: "Michelle Thomas",
+        city: "Seattle",
+        orderTotal: 99.99,
+        customerSince: new Date(2002, 10, 31)
+      },
+      {
+        id: 4,
+        name: "Jim Thomas",
+        city: "New York",
+        orderTotal: 599.99,
+        customerSince: new Date(2002, 10, 31)
+      }
+    ];
   }
 
   calculateOrders() {
@@ -22,6 +51,10 @@ export class CustomersListComponent implements OnInit {
     this.filteredCustomers.forEach((customer: ICustomer) => {
       this.customersOrderTotal += customer.orderTotal;
     });
+  }
+
+  sort(prop: string) {
+    this.filteredCustomers.sort(prop)
   }
 
 }
