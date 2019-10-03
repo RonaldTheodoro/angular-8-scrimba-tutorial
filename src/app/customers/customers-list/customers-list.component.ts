325a1f05d8faa5ae4c66d8a7698427397ef814bc
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2019-10-03 17:05:23 -0300

    Add table filter

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -23,6 +23,19 @@ export class CustomersListComponent implements OnInit {
     });
   }
 
+  filter(data: string) {
+    if (data) {
+      this.filteredCustomers = this.customers.filter((customer: ICustomer) => {
+        return customer.name.toLowerCase().indexOf(data.toLowerCase()) > -1 ||
+               customer.city.toLowerCase().indexOf(data.toLowerCase()) > -1 ||
+                            customer.orderTotal.toString().indexOf(data) > -1;
+      });
+      this.calculateOrders();
+    } else {
+      this.filteredCustomers = this.customers;
+    }
+  }
+
   sort(prop: string) {
     // this.filteredCustomers.sort(prop)
   }
--- src/app/customers/customers-list/filter-textbox.component.ts
+++ src/app/customers/customers-list/filter-textbox.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-    selector: 'app-filter-textbox',
-    templateUrl: './filter-textbox.component.html'
+  selector: "app-filter-textbox",
+  templateUrl: "./filter-textbox.component.html"
 })
 export class FilterTextboxComponent implements OnInit {
+  private _filter: string;
+  @Output() changed: EventEmitter<string> = new EventEmitter<string>();
 
-    ngOnInit() {
+  constructor() {}
 
-    }
+  ngOnInit() {}
+
+  @Input() get filter() {
+    return this._filter;
+  }
+
+  set filter(val: string) {
+    this._filter = val;
+    this.changed.emit(this.filter);
+  }
 
 }
