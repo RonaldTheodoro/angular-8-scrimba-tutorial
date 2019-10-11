2957bfb95666a55ecc56002a1e9de9807b9cf98b
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2019-10-11 15:12:11 -0300

    Calling the Server with HttpClient

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,16 +1,52 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
-import { Observable } from "rxjs/Observable";
+import { Observable } from "rxjs";
 import { map, catchError } from "rxjs/operators";
 
 import { ICustomer, IOrder } from "src/app/shared/interfaces";
 
 @Injectable()
 export class DataService {
-  baseUrl: string = "assets/";
+  private _baseUrl: string = "assets";
 
-  constructor() {}
+  constructor(private _http: HttpClient) {}
+
+  public getCustomers(): Observable<ICustomer[]> {
+    return this._http.get<ICustomer[]>(
+      `${this._baseUrl}/customers.json`
+    ).pipe(
+      catchError(this.handleError)
+    );   
+  }
+
+  public getCustomer(id: number): Observable<ICustomer> {
+    return this._http.get<ICustomer[]>(
+      `${this._baseUrl}/customers.json`
+    ).pipe(
+      map(customers => {
+        let customer = customers.filter(
+          (customer: ICustomer) => customer.id === id
+        );
+        return (customer && customers.length) ? customer[0] : null;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  public getOrders(id: number): Observable<IOrder[]> {
+    return this._http.get<IOrder[]>(
+      `${this._baseUrl}/orders.json`
+    ).pipe(
+      map(orders => {
+        let customerOrders = orders.filter(
+          (order: IOrder) => order.customerId === id
+        );
+        return customerOrders;
+      }),
+      catchError(this.handleError)
+    );
+  }
 
   private handleError(error: any) {
     console.error("Server error: ", error);
